0649a9e66e2efdd3c91b476e74033798e3d2178e
eekbk <eekeric@gmail.com>
2022-09-20 16:41:05 -0500

    update: add slider selector and conditional rendering to start/pause

--- client/components/Meditate.jsx
+++ client/components/Meditate.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Slider } from '@mui/material';
 
 const Meditate = () => {
   
   // initialize timer start value using useState
   const [num, setNum] = useState(600);
   // initialize a pause state value set to false
-  const [pause, setPause] = useState(false);
+  const [pause, setPause] = useState(true);
   
   // create a
   
-  const [randomInput, setRandomInput] = useState('');
+  //const [randomInput, setRandomInput] = useState('');
   const [timerVal, setTimerVal] = useState(600);
   // const renders = useRef(0);
   const timerId = useRef();
@@ -21,13 +22,13 @@ const Meditate = () => {
   //   count.current = count.current + 1;
   // });
   
-  // const handleChange = (e) => {
-  //   setRandomInput(e.target.value);
-  //   renders.current++;
-  // };
+  const handleSliderChange = (e) => {
+    setTimerVal(e.target.value * 60);
+  };
 
   // function to start the timer
   const startTimer = () => {
+    setPause(false);
     // set the interval to one second
     timerId.current = setInterval(() => {
       // renders.current++;
@@ -40,6 +41,7 @@ const Meditate = () => {
   const secondsView = seconds > 9 ? seconds : `0${seconds}`;
 
   const stopTimer = () => {
+    setPause(true);
     clearInterval(timerId.current);
     timerId.current = 0;
   };
@@ -63,16 +65,29 @@ const Meditate = () => {
       />
       <h4>Render Count: {renders.current}</h4> */}
       <br /><br />
-      <section>
-        <button onClick={startTimer}>Start</button>
-        <button onClick={stopTimer}>Pause</button>
-        <button onClick={resetTimer}>Reset</button>
-
-      </section>
       <br /><br />
       <p>Time Left: {`${minutes}:${secondsView}`}</p>
       <br /><br />
       {/* <p>{randomInput}</p> */}
+      <Slider
+        aria-label="Time"
+        defaultValue={10}
+        // getAriaValueText={'Time'}
+        valueLabelDisplay="auto"
+        onChange={handleSliderChange}
+        step={5}
+        marks
+        min={5}
+        max={60}
+      />
+      <section>
+        { pause ? <button onClick={startTimer}>Start</button> : <button onClick={stopTimer}>Pause</button>}
+        
+       
+        <button onClick={resetTimer}>Reset</button>
+
+      </section>
+      {/* <Slider defaultValue={0} step={1} marks min={1} max={2} /> */}
     </>
   );
 };
