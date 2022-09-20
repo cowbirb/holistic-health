8119145dac6d2283c5d8376ba7a29dcf7693b0d3
iamdestinos <iamdestinos@gmail.com>
2022-09-20 15:34:12 -0500

    update: moved graph items in profile visuals to graph component

--- client/components/profile-visuals/Graph.component.jsx
+++ client/components/profile-visuals/Graph.component.jsx
@@ -1,7 +1,52 @@
 import React, { useState } from 'react';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, } from 'chart.js'
+import { Chart } from 'react-chartjs-2';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, );
 
 const Graph = () => {
-
+    return (
+        <div>
+            <Chart
+            width={'100%'}
+            height={'100%'}
+            type='bar'
+            options={ 
+                {
+                    scales: 
+                        {
+                            y: {
+                                beginAtZero: true,
+                                yAxisKey: 'example'
+                            },
+                        },
+                    maintainAspectRatio: false
+                }
+            }
+            data={ 
+                {
+                    labels: [1, 2, 3, 4], 
+                    datasets: [
+                        {
+                            type: 'bar',
+                            label: 'Dataset 2',
+                            backgroundColor: 'rgb(75, 192, 192)',
+                            data: [1, 2, 3, 4].map(() => Math.floor(Math.random() * 10)),
+                            borderColor: 'white',
+                            borderWidth: 2,
+                            parsing: {
+                                y:
+                                    {
+                                        yAxisKey: 'example'
+                                    }
+                            },
+                        },
+                    ]
+                } 
+            }
+            ></Chart>
+        </div>
+    )
 }
 
 export default Graph;
--- client/components/profile-visuals/Profile-visuals.component.jsx
+++ client/components/profile-visuals/Profile-visuals.component.jsx
@@ -1,51 +1,10 @@
 import React, { useState } from 'react';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, } from 'chart.js'
-import { Chart } from 'react-chartjs-2';
+import Graph from './Graph.component.jsx';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, )
-//<canvas id="myChart" width="400" height="400"></canvas>
 const ProfileVisual = () => {
 
     return (
-        <div>
-            <Chart
-            width={'30%'}
-            type='bar'
-            options={ 
-                {
-                    scales: 
-                        {
-                            y: {
-                                beginAtZero: true,
-                                yAxisKey: 'example'
-                            },
-                        },
-                    maintainAspectRatio: false
-                }
-            }
-            data={ 
-                {
-                    labels: [1, 2, 3, 4], 
-                    datasets: [
-                        {
-                            type: 'bar',
-                            label: 'Dataset 2',
-                            backgroundColor: 'rgb(75, 192, 192)',
-                            data: [1, 2, 3, 4].map(() => Math.floor(Math.random() * 10)),
-                            borderColor: 'white',
-                            borderWidth: 2,
-                            parsing: {
-                                y:
-                                    {
-                                        yAxisKey: 'example'
-                                    }
-                            },
-                        },
-                    ]
-                } 
-            }
-            ></Chart>
-        </div>
+        <Graph></Graph>
     );
 }
 
