40b7e6220213f8d6e847f8099867d2509d49755e
iamdestinos <iamdestinos@gmail.com>
2022-09-20 19:36:46 -0500

    add: working images in summary

--- client/components/profile-visuals/Summary.component.jsx
+++ client/components/profile-visuals/Summary.component.jsx
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import birb from '../../../assets/cowbird-image.jpg';
+import baron from '../../../assets/Dore-munchausen-illustration.jpg';
 
 const Summary = () => {
 
+    return (
+        <div>
+            <Box sx={{ flexGrow: 1 }}>
+                <div style={{display: 'inline-block', 'vertical-align': 'top'}}>
+                    <img width='50' height='50' src={ birb } />
+                    <img width='75' height='100' src={ baron } />
+                </div>
+                <div style={{display: 'inline-block', float: 'center'}}>
+                    <p style={{float: 'center', color: 'red'}} className="float-right">testing text element</p>
+                </div>
+            </Box>
+        </div>
+    );
 };
 
 export default Summary;
