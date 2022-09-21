4a94ea13a0357c462458af8190a38331221ef059
jimenezfede <ricojimenez89@gmail.com>
2022-09-20 23:44:12 -0400

    added user context provider

--- client/index.jsx
+++ client/index.jsx
@@ -8,16 +8,19 @@ import Auth0ProviderWithNavigate from './auth/auth0-provider-with-navigate.jsx';
 import App from './components/App.jsx';
 import Profile from './components/Profile.jsx';
 import { AppContextProvider } from './context/AppContext.jsx';
+import {UserProvider} from './context/UserContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppContextProvider>
     <BrowserRouter>
       <Auth0ProviderWithNavigate>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route path='profile' element={<Profile />} />
-        </Routes>
+        <UserProvider>
+          <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='profile' element={<Profile />} />
+          </Routes>
+        </UserProvider>
       </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </AppContextProvider>
