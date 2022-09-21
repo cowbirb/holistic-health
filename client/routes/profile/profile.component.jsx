2f3f405a8e98499fb555803970a3dcafa3a67201
iamdestinos <iamdestinos@gmail.com>
2022-09-21 17:26:05 -0500

    add: profile displays profile visuals

--- client/routes/profile/profile.component.jsx
+++ client/routes/profile/profile.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ProfileVisual from '../../components/profile-visuals/Profile-visuals.component.jsx';
 
 const Profile = () => {
   return (
     <div>
       <h1>Profile</h1>
+      <ProfileVisual></ProfileVisual>
     </div>
   );
 };
