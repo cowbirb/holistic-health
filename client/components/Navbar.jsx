68276cb0dde3c846c9ab87e0032203a3f1d235c2
jimenezfede <ricojimenez89@gmail.com>
2022-09-20 23:47:42 -0400

    removed commented out code

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -26,18 +26,4 @@ function Navbar() {
   );
 }
 
-// function Navbar() {
-//   return (
-//     <nav
-//       style={{
-//         borderBottom: 'solid 1px',
-//         paddingBottom: '1rem',
-//       }}
-//     >
-//       <Link to='/'>Home </Link>
-//       <Link to='/profile'>Profile</Link>
-//     </nav>
-//   );
-// }
-
 export default Navbar;
