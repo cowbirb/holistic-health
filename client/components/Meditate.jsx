8a0af2f140bbb7341b0aeb02db9c3b80fc56b96e
eekbk <eekeric@gmail.com>
2022-09-21 00:14:38 -0500

    add: timer with functionality

--- client/components/Meditate.jsx
+++ client/components/Meditate.jsx
@@ -1,38 +1,77 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Slider } from '@mui/material';
+// import sound from '../media/audio/chime.wav';
 
 const Meditate = () => {
   
   // initialize timer start value using useState
-  const [num, setNum] = useState(600);
-  // initialize a pause state value set to false
+  const [userNum, setUserNum] = useState(600);
+  // initialize a pause state value set to true
   const [pause, setPause] = useState(true);
   
-  // create a
+  // create a state for if the timer is going off
+  const [isTimeUp, setIsTimeUp] = useState(false);
   
   //const [randomInput, setRandomInput] = useState('');
-  const [timerVal, setTimerVal] = useState(600);
+  const [timerVal, setTimerVal] = useState(userNum);
+  // set a value for if the timer is running
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
   // const renders = useRef(0);
   const timerId = useRef();
   
   // console.log(renders.current);
-  console.log('timerId.current:\n', timerId.current);
-  console.log('timerVal:\n', timerVal);
+  // console.log('timerId.current:\n', timerId.current);
+  // console.log('timerVal:\n', timerVal);
   // useEffect(() => {
   //   count.current = count.current + 1;
   // });
   
   const handleSliderChange = (e) => {
     setTimerVal(e.target.value * 60);
+    setUserNum(e.target.value * 60);
+  };
+  
+  const stopTimer = () => {
+    setPause(true);
+    setIsTimerRunning(false);
+    clearInterval(timerId.current);
+    timerId.current = 0;
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    setIsTimerRunning(false);
+    if (timerVal) {
+      //renders.current++;
+      setTimerVal(userNum);
+    }
   };
 
   // function to start the timer
   const startTimer = () => {
     setPause(false);
+    setIsTimerRunning(true);
     // set the interval to one second
     timerId.current = setInterval(() => {
       // renders.current++;
-      setTimerVal(prev => prev - 1);
+      setTimerVal((prev) => {
+        // if the previous value is gt 0
+        if (prev > 1) {
+          // countdown
+          return prev - 1;
+        // otherwise
+        } else {
+          // change the value of timesUp
+          setIsTimeUp(true);
+          // reset the timer
+          resetTimer();
+          // play the chime
+          // set a timout for 15 seconds until you switch the value of  timesup back
+          setTimeout(() => {
+            setIsTimeUp(false);
+          }, 15000);
+        }
+      });
     }, 1000);
   };
 
@@ -40,54 +79,79 @@ const Meditate = () => {
   const seconds = timerVal % 60;
   const secondsView = seconds > 9 ? seconds : `0${seconds}`;
 
-  const stopTimer = () => {
-    setPause(true);
-    clearInterval(timerId.current);
-    timerId.current = 0;
+
+  const timesUp = () => {
+    // const chime = new Audio('../media/audio/meditation-chime.mp3');
+    setIsTimeUp(true);
+    resetTimer();
+    //chime.play();
+    setTimeout(() => {
+      setIsTimeUp(false);
+    }, 18000);
   };
 
-  const resetTimer = () => {
-    stopTimer();
-    if (timerVal) {
-      //renders.current++;
-      setTimerVal(600);
+  // useEffect(() => {
+  //   console.log('we are inside of the useEffect');
+  //   resetTimer();
+  //   setIsTimeUp(true);
+  // });
+
+  //
+  const RenderView = () => {
+    // if the time isn't up
+    if (!isTimeUp) {
+      // if the timer is running
+      if (isTimerRunning) {
+        return (
+          <div>
+            <h2>
+              Breathing in, I calm my body.
+              <br></br>
+              <br></br>
+              Breathing out, I smile.
+              <br></br>
+              <br></br>
+              Dwelling in the present moment,
+              <br></br>
+              <br></br>
+              I know this is a wonderful moment.
+            </h2>
+          </div>
+        );
+      } else {
+        return <h2>{secondsView === '00' ? `${minutes} minutes` : `${minutes} minutes ${secondsView} seconds`}</h2>;
+
+      }
+
+    } else {
+      return <h2>Thank you.</h2>;
     }
   };
   
   return (
     <>
       <h1>Meditate</h1>
-      {/* <input
-        type="text"
-        value={randomInput}
-        placeholder="Random Input"
-        onChange={handleChange}
-      />
-      <h4>Render Count: {renders.current}</h4> */}
+      <br></br>
+      <br></br>
+      <RenderView />
       <br /><br />
       <br /><br />
-      <p>Time Left: {`${minutes}:${secondsView}`}</p>
-      <br /><br />
-      {/* <p>{randomInput}</p> */}
       <Slider
         aria-label="Time"
-        defaultValue={10}
+        // defaultValue={10}
+        value={timerVal / 60}
         // getAriaValueText={'Time'}
         valueLabelDisplay="auto"
         onChange={handleSliderChange}
         step={5}
         marks
-        min={5}
+        min={.1}
         max={60}
       />
       <section>
         { pause ? <button onClick={startTimer}>Start</button> : <button onClick={stopTimer}>Pause</button>}
-        
-       
         <button onClick={resetTimer}>Reset</button>
-
       </section>
-      {/* <Slider defaultValue={0} step={1} marks min={1} max={2} /> */}
     </>
   );
 };
