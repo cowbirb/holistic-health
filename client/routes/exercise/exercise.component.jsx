b145cf464f2d798540341f052ca5d2c0d5e2ba9f
jimenezfede <ricojimenez89@gmail.com>
2022-09-21 18:33:09 -0400

    moved code over from deleted file

--- client/routes/exercise/exercise.component.jsx
+++ client/routes/exercise/exercise.component.jsx
@@ -1,13 +1,69 @@
-import React from "react";
+import React, {useState, useContext, useEffect} from 'react';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import axios from 'axios';
+import {UserContext, setCurrentUser} from '../../context/user.context';
 
 const Exercise = () => {
+  const {currentUser} = useContext(UserContext);
+  const [newExercise, setNewExercise] = useState({
+    workout: '',
+    set: '',
+    rep: '',
+  });
+  console.log('user: ', currentUser);
+  useEffect(() => {
+    axios.get(`/api/user/${currentUser.email}`)
+    .then(({data}) => setCurrentUser(data))
+    .catch(err => console.log('exercise get unsuccessful', err));
+  }, [])
+
+  const exerciseChange = (e) => {
+    const {name, value} = e.target;
+    setNewExercise(newExercise => ({
+      ...newExercise,
+      [name]: value
+    }));
+  }
+  const updateWorkout = () => {
+    axios.put(`/profile/${currentUser.email}`, {users: {$push: {exercises: newExercise}}})
+    .then(() => setNewExercise({
+      workout: '',
+      set: '',
+      rep: '',
+    }))
+    .catch(err => console.log('workout update unsuccessful', err));
+  };
+
   return (
-    <>
-      <div>
-        <h1>Exercise</h1>
-      </div>
-    </>
-  );
-};
+    <Box>
+      <TextField
+        placeholder='name of exercise'
+        type='text'
+        name='workout'
+        value={newExercise.workout}
+        onChange={exerciseChange}
+      >Name of Exercise</TextField>
+      <TextField
+        placeholder='number of sets'
+        type='text'
+        name='set'
+        value={newExercise.set}
+        onChange={exerciseChange}
+      >Number of Sets</TextField>
+      <TextField
+        placeholder='number of reps'
+        type='text'
+        name='rep'
+        value={newExercise.rep}
+        onChange={exerciseChange}
+      >Number of Reps</TextField>
+      <button
+        type='submit'
+        onClick={updateWorkout}
+      >Update Workout</button>
+    </Box>
+  )
+}
 
 export default Exercise;
