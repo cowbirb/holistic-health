20d92d541abafa84174a1872c05a6e7298d2afe1
iamdestinos <iamdestinos@gmail.com>
2022-09-21 17:26:23 -0500

    add: select option

--- client/components/profile-visuals/Profile-visuals.component.jsx
+++ client/components/profile-visuals/Profile-visuals.component.jsx
@@ -3,10 +3,19 @@ import PieChart from './PieChart.component.jsx';
 import Summary from './Summary.component.jsx';
 
 const ProfileVisual = () => {
+    const [graphView, setGraphView] = useState('entries');
+
+    const selectHandler = (e) => {
+        setGraphView(e.target.value);
+    }
 
     return (
-        <div>
-            <PieChart></PieChart>
+        <div id="ProfileVisual">
+            <select value={graphView} onChange={selectHandler}>
+                <option value='entries' selected>Entries</option>
+                <option value='other'>Other</option>
+            </select>
+            {graphView === 'entries' ? <PieChart></PieChart> : <div>test</div>}
             <Summary></Summary>
         </div>
     );
