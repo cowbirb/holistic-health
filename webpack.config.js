17a2799d253abe66a44c28f3324a4198546299cd
jimenezfede <ricojimenez89@gmail.com>
2022-09-20 23:41:45 -0400

    added devtool

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
+  devtool: 'eval-source-map',
   entry: path.resolve(__dirname, 'client', 'index.jsx'),
   output: {
     filename: 'bundle.js',
