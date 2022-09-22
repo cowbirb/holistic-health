d79c2f6862a0bc123f387a6fbc18bc337c1bbb50
iamdestinos <iamdestinos@gmail.com>
2022-09-21 19:13:18 -0500

    add: set up profile info setup

--- client/routes/profile/profile.component.jsx
+++ client/routes/profile/profile.component.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ProfileVisual from '../../components/profile-visuals/Profile-visuals.component.jsx';
+import baron from '../../../assets/Dore-munchausen-illustration.jpg';
 
 const Profile = () => {
   return (
     <div>
-      <h1>Profile</h1>
+      <div id="profileInfo">
+        <h1>Profile</h1>
+        <div id="profileImage" style={{display: 'inline-block', verticalAlign: 'top', marginLeft: '5px'}}>
+          <img width='75' height='100' src={ baron }></img>
+        </div>
+        <div id="profileText" style={{display: 'inline-block', marginLeft: '10px'}}>
+          <p>{`INSERT_NAME_HERE`}</p>
+          <p>{`INSERT_EMAIL_HERE`}</p>
+        </div>
+      </div>
       <ProfileVisual></ProfileVisual>
     </div>
   );
