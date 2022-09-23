d618ef4dde54a8277111f932ce0793ce1dba4644
iamdestinos <iamdestinos@gmail.com>
2022-09-23 16:05:36 -0500

    update: hooked meditation chart to live data

--- client/components/profile-visuals/MeditationChart.component.jsx
+++ client/components/profile-visuals/MeditationChart.component.jsx
@@ -9,6 +9,11 @@ const MeditationChart = () => {
     const { currentUser } = useContext(UserContext);
     const [userInfo, setUserInfo] = useState(currentUser ? currentUser.daily_info : null);
     const [totalAvg, setTotalAvg] = useState(0);
+    const [veryHappyAvg, setVeryHappy] = useState(0);
+    const [happyAvg, setHappy] = useState(0);
+    const [neutralAvg, setNeutral] = useState(0);
+    const [angryAvg, setAngry] = useState(0);
+    const [veryAngryAvg, setVeryAngry] = useState(0);
     const [setup, setSetup] = useState(true);
 
     const calculateAvg = (arr) => {
@@ -20,10 +25,26 @@ const MeditationChart = () => {
         output /= 60;
         return output;
     }
+
+    const filterEntries = (str) => {
+        const output = userInfo.filter((curr, i, collection) => {
+            if (curr.emotion_of_the_day.did_respond) {
+                return curr.emotion_of_the_day.emotion === str;
+            } else {
+                return false;
+            }
+        });
+        return output;
+    }
     
     if (setup) {
         if (userInfo) {
             setTotalAvg(calculateAvg(userInfo));
+            setVeryHappy(calculateAvg(filterEntries('Very Happy')));
+            setHappy(calculateAvg(filterEntries('Happy')));
+            setNeutral(calculateAvg(filterEntries('Neutral')));
+            setAngry(calculateAvg(filterEntries('Angry')));
+            setVeryAngry(calculateAvg(filterEntries('Very Angry')));
         }
         setSetup(false);
     }
@@ -60,7 +81,7 @@ const MeditationChart = () => {
                         {
                             type: 'bar',
                             backgroundColor: ['seagreen', 'darkgreen', 'green', 'limegreen', 'lime', 'chartreuse'],
-                            data: [totalAvg, 1, 2, 3, 4, 5],
+                            data: [totalAvg, veryHappyAvg, happyAvg, neutralAvg, angryAvg, veryAngryAvg],
                             borderColor: ['seagreen', 'darkgreen', 'green', 'limegreen', 'lime', 'chartreuse'],
                             borderWidth: 2,
                         }
