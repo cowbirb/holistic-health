092d85a4ef60b9671cc204d9a3a92a444bc15df9
eekbk <eekeric@gmail.com>
2022-09-22 21:04:24 -0500

    add: GuidedMeditation component

--- client/routes/meditation/meditation.component.jsx
+++ client/routes/meditation/meditation.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Meditate from '../../components/Meditate.jsx';
+import GuidedMeditation from '../../components/GuidedMeditation.jsx';
 
 const Meditation = () => {
   return (
     <>
       <Meditate />
+      <GuidedMeditation />
     </>
   );
 };
