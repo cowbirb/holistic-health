4eccf4cd357de5bb5aa06761ed17cc6c3fb65764
wilsuriel03 <lacurly00@gmail.com>
2022-09-22 23:39:20 -0500

    updated save recipe endpoint

--- client/components/SavedRecipesList.jsx
+++ client/components/SavedRecipesList.jsx
@@ -6,19 +6,17 @@ import Container from "@mui/material/Container";
 import axios from "axios";
 
 const SavedRecipesList = ({ calorieCount, setCalorieCount, user }) => {
-
   const [recipes, setRecipes] = useState([]);
 
   console.log(recipes);
 
   useEffect(() => {
     axios
-      .get(`/api/user/myrecipes/${user.email}`)
+      .get(`/api/user/myrecipes/${user._id}`)
       .then(({ data }) => {
-        console.log(data);
-        if(data.length > 0){
-        setRecipes(data);
-        }   
+        if (data.length > 0) {
+          setRecipes(data);
+        }
       })
       .catch((err) => {
         console.log(err);
@@ -32,16 +30,17 @@ const SavedRecipesList = ({ calorieCount, setCalorieCount, user }) => {
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
       >
-        {recipes.length !== 0 && recipes.map((recipe, index) => (
-          <Grid item key={index} xs={2} sm={3} md={4}>
-            <SavedRecipe
-              savedRecipe={recipe}
-              key={index}
-              calorieCount={calorieCount}
-              setCalorieCount={setCalorieCount}
-            />
-          </Grid>
-        ))}
+        {recipes.length !== 0 &&
+          recipes.map((recipe, index) => (
+            <Grid item key={index} xs={2} sm={3} md={4}>
+              <SavedRecipe
+                savedRecipe={recipe}
+                key={index}
+                calorieCount={calorieCount}
+                setCalorieCount={setCalorieCount}
+              />
+            </Grid>
+          ))}
       </Grid>
     </Container>
   );
