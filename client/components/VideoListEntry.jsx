f5b1d8ab02bf1e9f5ad56752ee4231e205749479
eekbk <eekeric@gmail.com>
2022-09-23 17:31:26 -0500

    create: VideoListEntry component

---
+++ client/components/VideoListEntry.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const VideoListEntry = () => {
+  return (
+    <>
+      <h3>Video Name</h3>
+      <p>Video Description</p>
+      <p>Video Duration</p>
+    </>
+  )
+};
+
+export default VideoListEntry;
