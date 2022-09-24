223ed176a6820b55faabacceb0e7163e5e12653c
eekbk <eekeric@gmail.com>
2022-09-24 13:04:15 -0500

    add: handleMoreMeditations button, styling

--- client/components/GuidedMeditation.jsx
+++ client/components/GuidedMeditation.jsx
@@ -1,15 +1,27 @@
 import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
-//import { process } from 'dotenv'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import GuidedVideoEntry from './GuidedVideoEntry.jsx';
 import CurrentGuidedVideo from './CurrentGuidedVideo.jsx';
 
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Typography from "@mui/material/Typography";
+
 const GuidedMeditation = () => {
   // create states
-  const videoIndexStart = useRef();
+  const [videoIndexStart, setVideoIndexStart] = useState(0);
   const [videos, setVideos] = useState([]);
   const [currentVideoList, setCurrentVideoList] = useState([]);
-  const [currentVideo, setCurrentVideo] = useState(null);
+  // const [currentVideo, setCurrentVideo] = useState(null);
   // const [errorMessage, setErrorMessage] = useState()
   const [isLoading, setIsLoading] = useState(true);
 
@@ -32,11 +44,11 @@ const GuidedMeditation = () => {
   };
 
   useEffect(() => {
-    videoIndexStart.current = 0;
+    // videoIndexStart.current = 0;
     ( async () => {
       try {
         await getVideos();
-        setCurrentVideo(currentVideoList[0]);
+        // setCurrentVideo(currentVideoList[0]);
         // console.log('HELLOO? currentVideo?', currentVideo);
       } catch (err) {
         console.error('Son, you got an error in your IIFE:\n', err);
@@ -46,16 +58,17 @@ const GuidedMeditation = () => {
   }, []);
 
   useEffect(() => {
-    setCurrentVideoList(videos.slice(videoIndexStart.current, videoIndexStart.current + 5));
-    setCurrentVideo(currentVideoList[0]);
+    setCurrentVideoList(videos.slice(videoIndexStart, videoIndexStart + 10));
+    // setCurrentVideo(currentVideoList[0]);
     setIsLoading(false);
   }, [videos, videoIndexStart])
   
-  useEffect(() => {
-    setCurrentVideo(currentVideoList[0])
-  }, [currentVideoList])
+  // useEffect(() => {
+  //   setCurrentVideo(currentVideoList[0])
+  // }, [currentVideoList])
 
   const getVideos = async () => {
+    console.log('uh oh we sending a request');
     try {
       const { data: firstRequest } =  await axios.get('https://www.googleapis.com/youtube/v3/search?', notStateAxios)
       const videoIds = firstRequest.items.map(video => video.id.videoId).join(',');
@@ -75,17 +88,40 @@ const GuidedMeditation = () => {
         return video;
       });
       setVideos(completeVidInfo);
-      setCurrentVideoList(videos.slice(videoIndexStart.current, videoIndexStart.current + 5));
-      setCurrentVideo(videos[0]);
+      setCurrentVideoList(videos.slice(videoIndexStart, videoIndexStart + 10));
+      // setCurrentVideo(videos[0]);
     } catch (err) {
       console.error('The error from getVideos catch:\n', err);
     }
   }
-  return isLoading || !currentVideo ? <h2>Enjoy this moment</h2> : (
+
+  const handleMoreMeditations = () => {
+    // const { current } = videoIndexStart;
+    videoIndexStart < 50 ? setVideoIndexStart(videoIndexStart + 10) : setVideoIndexStart(0);
+  }
+
+  return isLoading ? <h2>Enjoy this moment</h2> : (
       <>
-    <h1>Guided Meditation</h1>
-    <CurrentGuidedVideo video={currentVideo} />
-    {currentVideoList.map((video, i) => <GuidedVideoEntry video={video} key={video.id.videoId} />)}
+      <Card>
+        <CardContent>
+    {/* <CurrentGuidedVideo video={currentVideo} />
+    {currentVideoList.map((video, i) => <GuidedVideoEntry video={video} key={video.id.videoId} />)} */}
+      <Carousel 
+        showIndicators={false} 
+        showThumbs={false} 
+        useKeyboardArrows={true} 
+        width={'75%'}
+        >
+              {currentVideoList.map(video => <CurrentGuidedVideo video={video} key={video.id.videoId} />)}
+              
+            </Carousel>
+          <CardActions>
+            <Button onClick={handleMoreMeditations}>
+              Load More Meditations
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
     </>
     );
 };
