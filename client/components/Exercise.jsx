75e74d2fa396c6871f52d5d213ea0c2e067cff70
jimenezfede <ricojimenez89@gmail.com>
2022-09-23 20:11:09 -0400

    added exercise component to render after mapping

---
+++ client/components/Exercise.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import {
+  styled,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Collapse,
+  IconButton,
+  Typography,
+  Button,
+  List,
+  ListItem,
+} from "@mui/material";
+
+const Exercises = () => (
+   <></>
+)
+
+export default Exercises;
