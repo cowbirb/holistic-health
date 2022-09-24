e12b58a4048401982175e005d4e7254d02b5054f
jimenezfede <ricojimenez89@gmail.com>
2022-09-24 14:48:10 -0400

    renamed exercise component

---
+++ client/components/ExerciseList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  styled,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Collapse,
+  IconButton,
+  Typography,
+  Button,
+  List,
+  ListItem,
+} from "@mui/material";
+
+const ExerciseList = ({workout: {workout, weight, set, reps}, daily_info}) => (
+  <Card  sx={{ maxWidth: 345, boxShadow: 7 }} className='workout-card' >
+    <CardHeader title={daily_info[daily_info.length - 1].date} />
+    <CardContent>
+      <List>
+        <ListItem>Type of workout: {workout}</ListItem>
+        <ListItem>Weight lifted: {weight}</ListItem>
+        <ListItem>Number of sets: {set}</ListItem>
+        <ListItem>Number of reps: {reps}</ListItem>
+      </List>
+    </CardContent>
+  </Card>
+);
+
+export default ExerciseList;
