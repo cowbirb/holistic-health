14341aa41f9a1021a33d5b3da9a901b89dd7a4da
wilsuriel03 <lacurly00@gmail.com>
2022-09-24 21:15:28 -0500

    feat: styled home page

--- client/routes/home/home.component.jsx
+++ client/routes/home/home.component.jsx
@@ -1,26 +1,39 @@
-import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import EmotionModal from "../../components/emotion-modal/emotion-modal.component";
-
-import {Carousel} from "react-responsive-carousel";
-
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/user.context";
 
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+import EmotionModal from "../../components/emotion-modal/emotion-modal.component";
+
+import {
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardHeader,
+  Divider,
+} from "@mui/material";
+
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
+import SelfImprovementIcon from "@mui/icons-material/SelfImprovement";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-// create corousel with inspirational quotes
 const Home = () => {
-    const { isAuthenticated } = useContext(UserContext);
+  const { isAuthenticated } = useContext(UserContext);
   const [quotes, setQuotes] = useState([]);
+
+  const navigate = useNavigate();
 
   const getQuotes = async () => {
     try {
-      const res = await axios.get("https://type.fit/api/quotes");
-      setQuotes(res.data);
+      const { data } = await axios.get("https://type.fit/api/quotes");
+      setQuotes(data);
     } catch (err) {
       console.log(err);
     }
@@ -30,136 +43,219 @@ const Home = () => {
     getQuotes();
   }, []);
 
-  console.log(quotes);
   return (
     <>
-      {/* a box centered horizontaly */}
-      {/* <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          width: "100vw",
-          backgroundColor: "#f5f5f5",
-        }}
-      >
-        <Card
+      {isAuthenticated ? (
+        <>
+          <Box
+            sx={{
+              marginTop: "5%",
+              width: "50%",
+              height: "300px",
+              marginLeft: "25%",
+              borderRadius: "10px",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              boxShadow: "0px 0px  2px grey",
+            }}
+          >
+            <Carousel
+              autoPlay={true}
+              infiniteLoop={true}
+              interval={5000}
+              showThumbs={false}
+              showStatus={false}
+              showIndicators={false}
+              showArrows={true}
+              stopOnHover={true}
+              transitionTime={1000}
+            >
+              {quotes.slice(0, 15).map((quote, indx) => (
+                <div key={indx}>
+                  <FormatQuoteIcon
+                    sx={{
+                      transform: "rotate(180deg)",
+                      fontSize: "50px",
+                      color: "#1976d2",
+                      marginBottom: "10px",
+                    }}
+                  />
+                  <h3 style={{ fontSize: 22 }}>{quote.text}</h3>
+                  <Divider
+                    sx={{ width: "50%", margin: "auto", color: "#1976d2" }}
+                  />
+                  <h4 style={{ fontSize: 16 }}>
+                    <i>{quote.author}</i>
+                  </h4>
+                </div>
+              ))}
+            </Carousel>
+            <EmotionModal />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "5%",
+            }}
+          >
+            <Grid
+              sx={{ flexGrow: 1, textAlign: "center", marginLeft: "265px" }}
+              container
+              spacing={2}
+            >
+              <Grid item>
+                <Card sx={{ width: "300px", height: "300px" }}>
+                  <CardHeader
+                    sx={{
+                      textAlign: "center",
+                      backgroundColor: "#1976d2",
+                      color: "white",
+                      borderRadius: "5px 5px 0 0",
+                    }}
+                    title="Meditate"
+                  />
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      <p>Take a deep breath</p>
+                    </Typography>
+                    <SelfImprovementIcon
+                      sx={{ fontSize: 50, cursor: "pointer" }}
+                      onClick={() => navigate("/meditation")}
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item>
+                <Card sx={{ width: "300px", height: "300px" }}>
+                  <CardHeader
+                    sx={{
+                      textAlign: "center",
+                      backgroundColor: "#1976d2",
+                      color: "white",
+                      borderRadius: "5px 5px 0 0",
+                    }}
+                    title="Exercise"
+                  />
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      <p>Set goals and track your progress</p>
+                    </Typography>
+                    <FitnessCenterIcon
+                      sx={{ fontSize: 50, cursor: "pointer" }}
+                      onClick={() => navigate("/exercise")}
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+
+              <Grid item>
+                <Card sx={{ width: "300px", height: "300px" }}>
+                  <CardHeader
+                    sx={{
+                      textAlign: "center",
+                      backgroundColor: "#1976d2",
+                      color: "white",
+                      borderRadius: "5px 5px 0 0",
+                    }}
+                    title="Meals"
+                  />
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      <p>Look for your favorites recipes and meals</p>
+                    </Typography>
+                    <RestaurantIcon
+                      sx={{ fontSize: 50, cursor: "pointer" }}
+                      onClick={() => navigate("/meals")}
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item>
+                <Card sx={{ width: "300px", height: "300px" }}>
+                  <CardHeader
+                    sx={{
+                      textAlign: "center",
+                      backgroundColor: "#1976d2",
+                      color: "white",
+                      borderRadius: "5px 5px 0 0",
+                    }}
+                    title="Journal"
+                  />
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      <p>Write down your thoughts and feelings</p>
+                    </Typography>
+                    <NewspaperIcon
+                      sx={{ fontSize: 50, cursor: "pointer" }}
+                      onClick={() => navigate("/journal")}
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Box>
+        </>
+      ) : (
+        <Box
           sx={{
-            width: "50%",
-            height: "50%",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            height: "100vh",
+            width: "100vw",
             backgroundColor: "#f5f5f5",
           }}
         >
-          <CardContent>
-            <Typography
-              sx={{
-                fontSize: 18,
-                color: "#000000",
-                fontWeight: "bold",
-              }}
-              color="text.secondary"
-              gutterBottom
-            >
-              Quote of the day
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: 22,
-                color: "#000000",
-                fontWeight: "bold",
-              }}
-              component="div"
-              color="text.secondary"
-              gutterBottom
-            >
-              {quotes.slice(0, 5).map((quote, indx) => (
-                <p key={indx}>{quote.text}</p>
-              ))}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Box> */}
-
-      {isAuthenticated ? (
-        
-          <><Carousel
-                  autoPlay={true}
-                  infiniteLoop={true}
-                  interval={5000}
-                  showThumbs={false}
-                  showStatus={false}
-                  showIndicators={false}
-                  showArrows={true}
-                  stopOnHover={true}
-                  transitionTime={1000}
-              >
-                  {quotes.slice(0, 10).map((quote, indx) => (
-                      <div key={indx}>
-                          <h3>{quote.text}</h3>
-                          <p>~{quote.author}</p>
-                      </div>
-                  ))}
-              </Carousel><EmotionModal /></>
-        ) : (
-            // a card centered in the middle of the page giving a small description of the app
-            <Box
+          <Card
+            sx={{
+              width: "50%",
+              height: "50%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#f5f5f5",
+            }}
+          >
+            <CardContent sx={{ textAlign: "center" }}>
+              <Typography
                 sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100vh",
-                    width: "100vw",
-                    backgroundColor: "#f5f5f5",
+                  fontSize: 22,
+                  color: "#1976d2",
+                  fontWeight: "bold",
                 }}
-            >
-                <Card
-                    sx={{
-                        width: "50%",
-                        height: "50%",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        backgroundColor: "#f5f5f5",
-                    }}
-                >
-                    <CardContent>
-                        <Typography
-                            sx={{
-                                fontSize: 18,
-                                color: "#000000",
-                                fontWeight: "bold",
-                            }}
-                            color="text.secondary"
-                            gutterBottom
-                        >
-                            Welcome to HolisticYou  
-                        </Typography>
-                        <br />
-                        <br />
-                        <Typography
-                            sx={{
-                                fontSize: 22,
-                                color: "#000000",
-                                fontWeight: "bold",
-                            }}
-                            component="div"
-                            color="text.secondary"
-                            gutterBottom
-                        >
-                            HolisticYou is a web application that allows you to track your emotions and thoughts.
-                            <br />
-                            <br />
-                            You can also view your emotions and thoughts over time to see how you are feeling.
-                            
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Box>
-        )}
+                color="text.secondary"
+                gutterBottom
+              >
+                Welcome to HolisticYou
+              </Typography>
+    
+              <br />
+              <Typography
+                sx={{
+                  fontSize: 18,
+                  color: "#000000",
+                  fontWeight: "bold",
+                }}
+                component="div"
+                color="text.secondary"
+                gutterBottom
+              >
+                <p> A place to help you take care of yourself</p>
+                <p>
+                  HolisticYou keeps track of your physical, mental and emotional
+                  patterns and practices, and aims to show you a representation
+                  of You, in this moment.
+                </p>
+              </Typography>
+                <span>Please Log In for a full Holistic Experience</span>
+            </CardContent>
+          </Card>
+        </Box>
+      )}
     </>
   );
 };
